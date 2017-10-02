4c0d577ba90af206338c6e533d1c2be5545a65ad
Yuya Takashina <takashina2051@gmail.com>
2017-10-02 17:39:15 +0900

    modified:   ulsif.py

--- ulsif.py
+++ ulsif.py
@@ -3,32 +3,96 @@ import scipy.stats
 import sklearn.cluster
 
 
+def norm(X, mean, sigma):
+    cov = sigma ** 2 * np.eye(len(mean))
+    return scipy.stats.multivariate_normal.pdf(X, mean=mean, cov=cov)
+
+
+def mutual_information(X, Y, Z=None, b=100, sigma=1, lamb=1e-3):
+    n_x, d_x = X.shape
+    n_y, d_y = Y.shape
+    b = min(b, n_x)
+    
+    if Z is None:
+        XY = np.hstack([X, Y])
+        UV = sklearn.cluster.KMeans(b).fit(XY).cluster_centers_
+        U, V = np.split(UV, [d_x], axis=1)
+        phi_x = np.array([norm(X, u, sigma) for u in U])
+        phi_y = np.array([norm(Y, v, sigma) for v in V])
+        phi = phi_x * phi_y
+        H_x = phi_x.dot(phi_x.transpose()) / n_x
+        H_y = phi_y.dot(phi_y.transpose()) / n_x
+        H = H_x * H_y
+        h = np.mean(phi, axis=1)
+        
+    else:
+        XYZ = np.hstack([X, Y, Z])
+        UVW = sklearn.cluster.KMeans(b).fit(XYZ).cluster_centers_
+        U, V, W = np.split(UVW, [d_x, d_x + d_y], axis=1)
+        phi_x = np.array([norm(X, u, sigma) for u in U])
+        phi_y = np.array([norm(Y, v, sigma) for v in V])
+        phi_z = np.array([norm(Z, w, sigma) for w in W])
+        phi = phi_x * phi_y * phi_z
+        H_x = phi_x.dot(phi_x.transpose()) / n_x
+        H_y = phi_y.dot(phi_y.transpose()) / n_x
+        H_z = phi_z.dot(phi_z.transpose()) / n_x
+        H = H_x * H_y * H_z
+        h = np.mean(phi, axis=1)
+        
+    alpha = np.linalg.solve(H + lamb * np.eye(b), h)
+    alpha[alpha < 0] = 0
+    return np.mean(np.log(alpha.dot(phi)))
+
+
 class MutualInformation(object):
     def __init__(self, sigma=1, lamb=1e-6, b=100):
         self._sigma = sigma
         self._lambda = lamb
         self._b = b
         
-    
-    def fit(self, X, Y):
+        
+    def fit(self, X, Y, Z=None):
         n_x, d_x = X.shape
         n_y, d_y = Y.shape
-        if n_x != n_y:
-            raise Exception()
+        b = min(self._b, n_x)
+        
+        if Z is None:
+            XY = np.hstack([X, Y])
+            UV = sklearn.cluster.KMeans(b).fit(XY).cluster_centers_
+            U, V = np.split(UV, [d_x], axis=1)
+            
+            phi_x = np.array([norm(X, u, self._sigma) for u in U])
+            phi_y = np.array([norm(Y, v, self._sigma) for v in V])
+            phi = phi_x * phi_y
+            H_x = phi_x.dot(phi_x.transpose()) / n_x
+            H_y = phi_y.dot(phi_y.transpose()) / n_x
+            H = H_x * H_y
+            h = np.mean(phi, axis=1)
+            #h_x = np.sum(phi_x, axis=1)
+            #h_y = np.sum(phi_y, axis=1)
+            #h_xy = np.sum(phi, axis=1)
+            #h = (h_x * h_y - h_xy) / (n_x ** 2 - n_x)
+            
+        else:
+            XYZ = np.hstack([X, Y, Z])
+            UVW = sklearn.cluster.KMeans(b).fit(XYZ).cluster_centers_
+            U, V, W = np.split(UVW, [d_x, d_x + d_y], axis=1)
+            
+            phi_x = np.array([norm(X, u, self._sigma) for u in U])
+            phi_y = np.array([norm(Y, v, self._sigma) for v in V])
+            phi_z = np.array([norm(Z, w, self._sigma) for w in W])
+            phi = phi_x * phi_y * phi_z
+            H_x = phi_x.dot(phi_x.transpose()) / n_x
+            H_y = phi_y.dot(phi_y.transpose()) / n_x
+            H_z = phi_z.dot(phi_z.transpose()) / n_x
+            H = H_x * H_y * H_z
+            h = np.mean(phi, axis=1)
+            #h_x = np.sum(phi_x, axis=1)
+            #h_y = np.sum(phi_y, axis=1)
+            #h_z = np.sum(phi_z, axis=1)
+            #h_xyz = np.sum(H, axis=0)
+            #h = (h_x * h_y * h_z - h_xyz) / (n_x ** 3 - n_x)
 
-        XY = np.hstack([X, Y])
-        n, d = XY.shape
-        b = min(self._b, n)
-        
-        UV = sklearn.cluster.KMeans(b).fit(XY).cluster_centers_
-        U, V = np.split(UV, [d_x], axis=1)
-        
-        phi_x = np.array([norm(X, u, self._sigma) for u in U])
-        phi_y = np.array([norm(Y, v, self._sigma) for v in V])
-        h = np.mean(phi_x * phi_y, axis=1)
-        H_x = phi_x.dot(phi_x.transpose()) / n
-        H_y = phi_y.dot(phi_y.transpose()) / n
-        H = H_x * H_y
 
         alpha = np.linalg.solve(H + self._lambda * np.eye(b), h)
         alpha[alpha < 0] = 0
